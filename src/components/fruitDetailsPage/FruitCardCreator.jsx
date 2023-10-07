f5f20d1a939cd45f68eb0422c891ca911381826c
jruedat@unal.edu.co <jruedat@unal.edu.co>
2023-10-06 22:00:08 -0500

    update

--- src/components/fruitDetailsPage/FruitCardCreator.jsx
+++ src/components/fruitDetailsPage/FruitCardCreator.jsx
@@ -11,26 +11,9 @@ export const FruitCardCreator = ({ data }) => {
                 {data.map((json) => {
                     i += 1;
                     return (
-                        <Grid
-                            item
-                            xs={5.5}
-                            sm={5.2}
-                            md={4}
-                            lg={3.55}
-                            xl={2.7}
-                            key={json.id}
-                            style={{ marginBottom: "80px" }}
-                        >
-                            <Link
-                                key={json.id}
-                                to={`/Fruit?name=${json.name}&color=${json.color}&power=${json.power}&eater=${json.eater}&type=${json.type}&image=${json.image}&description_fruit=${json.description_fruit}`}
-                            >
-                                <FruitCard
-                                    key={json.id}
-                                    name={json.name}
-                                    image={json.image}
-                                    description={json.description_card}
-                                />
+                        <Grid item xs={5.5} sm={5.2} md={4} lg={3.55} xl={2.7} key={json.id} style={{ marginBottom: "80px" }}>
+                            <Link key={json.id} to={`/Fruit?name=${json.name}&color=${json.color}&power=${json.power}&eater=${json.eater}&type=${json.type}&image=${json.image}&description_fruit=${json.description_fruit}`}>
+                                <FruitCard key={json.id} name={json.name} image={json.image} description={json.description_card} />
                             </Link>
                         </Grid>
                     );
